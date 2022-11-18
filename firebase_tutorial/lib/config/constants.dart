1176372073391b9423dfdc16acfbcce43fdfc7ff
ThanhTrung2001 <75150646+ThanhTrung2001@users.noreply.github.com>
2022-11-18 15:38:07 +0700

    Update constants.dart

--- firebase_tutorial/lib/config/constants.dart
+++ firebase_tutorial/lib/config/constants.dart
@@ -1,5 +1,5 @@
 int tokenRole = 1; // use 1 for Host/Broadcaster, 2 for Subscriber/Audience
 String serverUrl = "https://agora-token-service-production-92ff.up.railway.app"; 
-// String serverUrl = "localhost:8080"; // The base URL to your token server, for example "https://agora-token-service-production-92ff.up.railway.app"
+// String serverUrl = "localhost:8080"; // The base URL to your token server, for example "https://agora-token-service-production-92ff.up.railway.app" => use for NodeJS
 int tokenExpireTime = 86400; // Expire time in Seconds.
 bool isTokenExpiring = false; // Set to true when the token is about to expire
